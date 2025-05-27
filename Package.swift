2fbbe42642e1c34a3c4fc7b2a84142ec38f1b5bf
YehorKyrylov <>
2025-05-27 18:37:28 +0200

    remove ,

--- Package.swift
+++ Package.swift
@@ -39,7 +39,7 @@ let package = Package(
         .target(
             name: "HapticClientSwiftUI",
             dependencies: ["HapticClient"],
-            path: "Sources/SwiftUISupport",
+            path: "Sources/SwiftUISupport"
         ),
         .target(
             name: "HapticClientDependencies",
